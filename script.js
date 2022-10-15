8bc93dbacad840a3c425265423bfbd0fc7763252
Justin Harris <justin.r.harris@gmail.com>
2022-10-15 11:59:42 -0400

    added h1 font

--- script.js
+++ script.js
@@ -14,15 +14,16 @@ async function getData(event) {
   event.preventDefault();
   let textInput = document.querySelector("#inputBar").value;
 
-  fetch(`https://www.breakingbadapi.com/api/characters/${textInput}`)
+  fetch(`https://www.breakingbadapi.com/api/characters?name=${textInput}`)
     .then((res) => {
       return res.json();
     })
     .then((res) => {
-      console.log(res[0].name);
-      bbName.innerText = res[0].name;
+      //console.log(res[0].name);
+      bbName.innerHTML = `Name: ${res[0].name}`;
       bbImage.setAttribute("src", res[0].img);
-      bbInfo.innerText = `${res.height}: ft and ${res.weight} lbs`;
+      bbBirthday.innerText = `Birthdate: ${res[0].birthday}`;
+      //bbInfo.innerText = `${res.height}: ft and ${res.weight} lbs`;
       //bbBio.innerText = bbGroup.innerText = console.log(res.sprites);
     })
     .catch((err) => {
